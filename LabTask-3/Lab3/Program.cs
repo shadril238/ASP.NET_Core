5c96cbd4fb52432ee6f532f5e070cbac7a729697
shadril238 <shadrilhassan@outlook.com>
2022-02-16 02:02:00 +0600

    updated lab task 3

---
+++ LabTask-3/Lab3/Program.cs
@@ -0,0 +1,57 @@
+﻿//Author: Shadril Hassan Shifat [20-42451-1] 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Object for Student Class
+            Student shadril = new Student();
+            shadril.Name = "Shadril Hassan Shifat";
+            shadril.Id = "20-42451-1";
+            shadril.Department = "CSE";
+            shadril.Cgpa = 3.95f;
+            shadril.ShowInfo();
+            Console.WriteLine();//newline
+
+            //Object for Triangle class
+            Triangle triangle = new Triangle();
+            triangle.X = 6;
+            triangle.Y = 7;
+            triangle.Z = 6;
+            triangle.ShowInfo();
+            triangle.TestTriangle();
+            Console.WriteLine();//newline
+
+            //Object for Account class
+            Account account = new Account();
+            account.AccName = "Shadril Hassan";
+            account.Acid = "SMSA-20424511";
+            account.Balance = 5000;
+            Console.WriteLine("Account Balance : " + account.Balance);
+            account.Deposit(3000);
+            account.Withdraw(500);
+
+            Account account1 = new Account("Leen", "SMSA-121020", 1000);
+            account.Transfer(2000, account1);//transfer from account to account1
+            Console.WriteLine(); ///Account Balace will be shown here.
+
+            //Console.WriteLine();//newline
+
+            //Object for Course class
+            Course course = new Course();
+            course.CourseName = "OOP2-C#";
+            course.CourseCode = "DN0109";
+            course.courseCredit = 3;
+            course.ShowCourseInfo();
+            Console.WriteLine();//newline
+        }
+    }
+}
---
+++ LabTask-3/Lab3/Triangle.cs
@@ -0,0 +1,76 @@
+﻿//Author: Shadril Hassan Shifat [20-42451-1] 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal class Triangle
+    {
+        private int x, y, z;
+        public Triangle()
+        {
+            Console.WriteLine("Default constructor of Triangle class is called.");
+        }
+
+        public int X
+        {
+            get 
+            { 
+                return x; 
+            }
+            set 
+            { 
+                x = value; 
+            }
+        }
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                y = value;
+            }
+        }
+        public int Z
+        {
+            get
+            {
+                return z;
+            }
+            set
+            {
+                z = value;
+            }
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Size X of the triangle : " + this.x);
+            Console.WriteLine("Size Y of the triangle : " + this.y);
+            Console.WriteLine("Size Z of the triangle : " + this.z);
+
+        }
+        public void TestTriangle()
+        {
+            if(this.x == this.y && this.y == this.z)
+            {
+                Console.WriteLine("The triangle is equilateral.");
+            }
+            else if(this.x == this.y || this.y == this.z || this.x == this.z)
+            {
+                Console.WriteLine("The triangle is isosceles.");
+            }
+            else
+            {
+                Console.WriteLine("The triangle is scalene.");
+            }
+        }
+
+    }
+}
