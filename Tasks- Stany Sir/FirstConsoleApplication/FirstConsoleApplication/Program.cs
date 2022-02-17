badb62c0b85fbe0380e8f2e1feab84a2ea9b2b9b
shadril238 <shadrilhassan@outlook.com>
2022-02-17 17:56:06 +0600

    added task of stany sir

---
+++ Tasks- Stany Sir/FirstConsoleApplication/FirstConsoleApplication/Program.cs
@@ -0,0 +1,13 @@
+﻿//Author: shadril238
+
+namespace FirstConsoleApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///System.Console.WriteLine("Hello World");
+            System.Console.WriteLine("Hello shadril238!");
+        }
+    }
+}
---
+++ Tasks- Stany Sir/Task1/ClassLibrary_Task1/Student.cs
@@ -0,0 +1,37 @@
+﻿//Author: shadril238
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary_Task1
+{
+    public class Student
+    {
+        public int sem;
+        public string name, id;
+        public float cgpa;
+
+        public Student()
+        {
+
+        }
+
+        public void setValues(int sem, string id, string name, float cgpa)
+        {
+            this.sem = sem;
+            this.id = id;
+            this.name = name;
+            this.cgpa = cgpa;
+            
+        }
+
+        public void displayValues()
+        {
+            Console.WriteLine("Name : " + name + "\nID : " + id + "\nCGPA : " + cgpa + "\nTotal Semester : " + sem);
+        }
+
+    }
+}
