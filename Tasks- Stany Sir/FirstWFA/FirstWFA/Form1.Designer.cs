6a7d936ac2fa170f59f56d62c4f1592009245fcc
shadril238 <shadrilhassan@outlook.com>
2022-04-13 05:10:31 +0600

    practice WFA

---
+++ Tasks- Stany Sir/FirstWFA/FirstWFA/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace FirstWFA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(125, 113);
+            this.txt1.Multiline = true;
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(233, 45);
+            this.txt1.TabIndex = 0;
+            this.txt1.TextChanged += new System.EventHandler(this.txt1_TextChanged);
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(125, 217);
+            this.txt2.Multiline = true;
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(233, 45);
+            this.txt2.TabIndex = 1;
+            this.txt2.TextChanged += new System.EventHandler(this.txt2_TextChanged);
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.White;
+            this.btn1.Font = new System.Drawing.Font("Modern No. 20", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(125, 301);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(233, 61);
+            this.btn1.TabIndex = 2;
+            this.btn1.Text = "Sum";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.Location = new System.Drawing.Point(413, 113);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(236, 45);
+            this.lbl1.TabIndex = 3;
+            // 
+            // lbl2
+            // 
+            this.lbl2.Location = new System.Drawing.Point(413, 220);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(236, 45);
+            this.lbl2.TabIndex = 4;
+            // 
+            // lbl3
+            // 
+            this.lbl3.Location = new System.Drawing.Point(413, 301);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(236, 45);
+            this.lbl3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(902, 508);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "First WFA";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+    }
+}
+
---
+++ Tasks- Stany Sir/FirstWFA/FirstWFA/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstWFA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit(); //Exit Application
+        }
+
+        private void txt1_TextChanged(object sender, EventArgs e)
+        {
+            lbl1.Text = "You typed: " + txt1.Text; //Whatever typing in TextBox, will automatically change the label
+        }
+
+        private void txt2_TextChanged(object sender, EventArgs e)
+        {
+            lbl2.Text = "You typed: " + txt2.Text;
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            int x1= Int32.Parse(txt1.Text);
+            int x2= Int32.Parse(txt2.Text);
+            int sum = x1 + x2;
+            lbl3.Text = "Sum = " + sum;//When button clicked, the label is also changed
+            MessageBox.Show("Sum = " + sum);
+        }
+
+        
+    }
+}
