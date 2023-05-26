3eab5d379f2b5774edc276711c4506c07ead257c
shadril238 <shadrilhassan@outlook.com>
2023-05-27 04:19:37 +0600

    API Versioning - (till 31)

---
+++ ApiVersioning_Example/ApiVersioning_Example/Controllers/UserV1Controller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiVersioning_Example.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    public class UserV1Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetInfo()
+        {
+            return Ok("User V1 Controller");
+        }
+    }
+}
---
+++ ApiVersioning_Example/ApiVersioning_Example/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// for Api versioning (shadril238)
+builder.Services.AddApiVersioning();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// for Api versioning (shadril238)
+app.UseApiVersioning();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
