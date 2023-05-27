6808363e6fab1f12cfed9a753797fa4fbe4ba79b
shadril238 <shadrilhassan@outlook.com>
2023-05-28 05:13:10 +0600

    BookEmpire init : ConnString, EFCore

---
+++ BookEmpire/BookEmpire/Contexts/BookEmpireDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookEmpire.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookEmpire.Contexts
+{
+    public class BookEmpireDbContext : DbContext
+    {
+        public BookEmpireDbContext(DbContextOptions<BookEmpireDbContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
