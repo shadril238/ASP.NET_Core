4ef7be35de93c48dbe2eb0effc95b2692859a656
shadril238 <shadrilhassan@outlook.com>
2023-05-30 05:06:02 +0600

    Razor - BookEmpire Razor init

---
+++ BookEmpire/BookEmpireRazor_Temp/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BookEmpireRazor_Temp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [Required]
+        [DisplayName("Display Order")]
+        [Range(1, 100)]
+        public int DisplayOrder { get; set; }
+    }
+}
